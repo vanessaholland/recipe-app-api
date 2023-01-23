28c1846cb205f0562c1b5dcb25f5735b61a926e1
VHolland <vanessaholland13@gmail.com>
2023-01-23 06:23:03 -0600

    user management (#6)
    adding user management

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,38 @@
+from django.contrib.auth import get_user_model
+from django.test import Client
+from django.test import TestCase
+from django.urls import reverse
+
+
+class AdminSiteTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email = 'adminuser@example.com',
+            password = 'testpass123',
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email = 'user@example.com',
+            password = 'testpass123',
+            name = 'Test User'
+        )
+
+    def test_users_list(self):
+        url = reverse('admin:core_user_changelist')
+        result = self.client.get(url)
+
+        self.assertContains(result, self.user.email)
+        self.assertContains(result, self.user.name)
+
+    def test_user_edit_page(self):
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        result = self.client.get(url)
+
+        self.assertEqual(result.status_code, 200)
+
+    def test_user_create_page(self):
+        url = reverse('admin:core_user_add')
+        result = self.client.get(url)
+
+        self.assertEqual(result.status_code, 200)
