dca8084831f52a0f2f3b9ba5bd30009f5aea3391
VHolland <vanessaholland13@gmail.com>
2023-01-30 08:37:43 -0600

    adding token auth (#9)
    adding auth token

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -1,5 +1,9 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import (
+    authenticate,
+    get_user_model,
+)
 from rest_framework import serializers
+from django.utils.translation import gettext as _
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -11,4 +15,26 @@ class UserSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         return get_user_model().objects.create_user(**validated_data)
+
+class AuthTokenSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(
+        style = {'input_type': 'password'},
+        trim_whitespace = False,
+    )
+
+    def validate(self, attrs):
+        email = attrs.get('email')
+        password = attrs.get('password')
+        user = authenticate(
+            request = self.context.get('request'),
+            username = email,
+            password = password
+        )
+        if not user:
+            message = _('Unable to authenticate with provided credentials')
+            raise serializers.ValidationError(message, code='authorization')
+
+        attrs['user'] = user
+        return attrs
 
