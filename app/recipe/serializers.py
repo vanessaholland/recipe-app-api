1e760d58c48844e2f5a48fed53b291bcf2673c34
Vanessa Holland <vanessaholland13@gmail.com>
2023-10-06 13:30:42 -0500

    lint

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -4,11 +4,10 @@ from core.models import Recipe
 
 
 class RecipeSerializer(serializers.ModelSerializer):
-    """Serializer for recipe objects"""
 
     class Meta:
         model = Recipe
         fields = [
             'id', 'title', 'time_minutes', 'price', 'link', 'description'
         ]
-        read_only_fields = ['id',]
+        read_only_fields = ['id', ]
