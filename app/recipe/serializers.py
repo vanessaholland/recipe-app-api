0eac38b170a3a4635bc976e9c62846f9e9938cb9
Vanessa Holland <vanessaholland13@gmail.com>
2023-10-06 13:04:30 -0500

    wip

---
+++ app/recipe/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+
+from core.models import Recipe
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    """Serializer for recipe objects"""
+
+    class Meta:
+        model = Recipe
+        fields = (
+            'id', 'title', 'time_minutes', 'price', 'link', 'description'
+        )
+        read_only_fields = ('id',)
