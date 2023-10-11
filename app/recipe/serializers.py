2fa2db1dc83e63ddcffbed0888afef828f727b9c
Vanessa Holland <vanessaholland13@gmail.com>
2023-10-11 13:10:42 -0500

    adding recipe detail endpoint

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -11,3 +11,8 @@ class RecipeSerializer(serializers.ModelSerializer):
             'id', 'title', 'time_minutes', 'price', 'link', 'description'
         ]
         read_only_fields = ['id', ]
+
+
+class RecipeDetailSerializer(RecipeSerializer):
+    class Meta(RecipeSerializer.Meta):
+        fields = RecipeSerializer.Meta.fields + ['description']
